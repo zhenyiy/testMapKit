b08525e6550381f17f4a962b79737cfc24f86744
Zhenyi Yang <zhenyiy@gmail.com>
2022-07-27 19:43:47 -0400

    Initial Commit

---
+++ testMapKit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testMapKit
+//
+//  Created by Zhenyi Yang on 7/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testMapKit/testMapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testMapKitApp.swift
+//  testMapKit
+//
+//  Created by Zhenyi Yang on 7/27/22.
+//
+
+import SwiftUI
+
+@main
+struct testMapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
